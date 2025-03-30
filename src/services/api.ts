adafd20f96707266bc28719e29dc0d98f47f20bc
siakagassama <siakag@gmail.com>
2025-03-30 14:14:49 -0600

    brewhaus-demo - pull env for api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import type { Brewery } from '@/types'
 import axios from "axios";
 
-const BASE_URL = "https://api.openbrewerydb.org/v1/breweries";
+const BASE_URL = import.meta.env.VITE_BREWERIES_API_BASE_URL;
 
 export const fetchBreweries = async (page = 1, perPage = 10, query = ""): Promise<Brewery[]> => {
   const params: { page?: number; per_page?: number; query?: string } = {};
